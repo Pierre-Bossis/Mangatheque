562a827953866377859fed156c4317bdd4a54a7c
Pierre-Bossis <pbossis@hotmail.com>
2022-12-12 00:18:18 +0100

    commit 6

--- Mangatheque.Web.UI/Pages/MangaDetail.cshtml.cs
+++ Mangatheque.Web.UI/Pages/MangaDetail.cshtml.cs
@@ -1,3 +1,5 @@
+using Mangatheque.Core.Interfaces.Repositories;
+using Mangatheque.Core.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
@@ -5,8 +7,37 @@ namespace Mangatheque.Web.UI.Pages
 {
     public class MangaDetailModel : PageModel
     {
-        public void OnGet()
+        #region Fields
+        private readonly IMangaRepository repository;
+        #endregion
+        #region Constructors
+        public MangaDetailModel(IMangaRepository repository)
         {
+            this.repository = repository;
         }
+        #endregion
+        #region Public Methods
+        public IActionResult OnGet()
+        {
+            IActionResult result = this.Page();
+
+            try
+            {
+                this.manga = this.repository.GetOne(this.Id);
+            }
+            catch (Exception)
+            {
+
+                result = this.NotFound();
+            }
+            return result;
+        }
+        #endregion
+        #region Properties
+        [BindProperty(SupportsGet = true)]
+        public int Id { get; set; }
+
+        public Manga? manga { get; set; }
+        #endregion
     }
 }
